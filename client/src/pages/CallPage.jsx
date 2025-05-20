e9ba2671c047b3d9b6e0feee4e8208b7ec6588d2
Distrustfultie <efecherebor22@gmail.com>
2025-05-20 15:50:16 +0100

    fix

--- client/src/pages/CallPage.jsx
+++ client/src/pages/CallPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router"
 import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStreamToken } from "../lib/api";
+import PageLoader from "../components/PageLoader";
 
 import {
   StreamVideo,
